275662e26d81a43233f68e0d3774a31fa9741e16
Ashel Vasquez <vasquezpalominoashel@gmail.com>
2022-10-16 13:12:56 -0500

    Ampliando el controlador de los productos

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,23 +1,46 @@
 const router = require('express').Router();
 const { Op } = require('sequelize');
 
-const { Product } = require('../models');
+const { Product, Category } = require('../models');
 
 router.get('/', async (req, res) => {
-  const where = {};
+  try {
+    const where = {};
+  
+    if (req.query.search) {
+      where.name = {
+        [Op.substring]: req.query.search,
+      };
+    }
+  
+    const products = await Product.findAll({
+      attributes: { exclude: ['id', 'category'] },
+      include: {
+        model: Category,
+        as: 'type',
+        attributes: ['name'],
+        where: {
+          name: req.query.category ? req.query.category : { [Op.ne]: null },
+        }
+      },
+      where,
+    });
 
-  if (req.query.search) {
-    where.name = {
-      [Op.substring]: req.query.search,
-    };
+    if (!products.length) {
+      throw new Error('Ningún producto encontrado');
+    }
+  
+    return res.json({
+      status: 'success',
+      data: products,
+    });
+    
+  } catch (error) {
+    return res.status(404).json({
+      status: 'error',
+      message: error.message,
+    });
   }
-
-  const products = await Product.findAll({
-    attributes: { exclude: ['id', 'category'] },
-    where,
-  });
-
-  res.json(products);
 });
 
 module.exports = router;
